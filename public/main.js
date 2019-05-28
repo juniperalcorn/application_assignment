beac469bac07307fce8ee9f63c15969e4c4e05c1
junipera <juniper.alcorn@gmail.com>
2019-05-28 13:50:49 -0400

    install package and update readme

--- public/main.js
+++ public/main.js
@@ -1,5 +1,10 @@
-fetch('/quote')
-  .then(r => r.json())
-  .then(data => {
-    document.getElementById('quote').innerText = data.quote;
-  });
+fetch(`http://localhost:3000/quote`, {
+  headers: {
+    "Access-Control-Allow-Origin" : "*", 
+  }
+})
+.then(r => r.json())
+.then(data => {
+  document.getElementById("quote").innerText = data.quote;
+})
+.catch(err=>console.log(err));
